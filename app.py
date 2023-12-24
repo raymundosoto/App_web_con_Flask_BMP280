c438c9d4fd66d96e73fcdba5ace4d7cb1b89f34c
Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2023-12-24 15:34:10 -0600

    Update app.py
    corección a json

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
+
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
+
+datos_recibidos = []
 
 @app.route('/datos', methods=['POST'])
 def recibir_datos():
     data = request.get_json()
     print("Datos recibidos:", data)
+
+    datos_recibidos.append(data)  # Agregar los datos al registro
+
     return jsonify({"mensaje": "Datos recibidos correctamente"})
 
 @app.route('/grafica')
 def obtener_datos_grafica():
-    # Datos de prueba con el formato del JSON del ESP32
-    data = {
-        'id_sensor': 'bmp280',
-        'timestamp': '2023-12-24 20:28:04',
-        'temperatura': 20.65999985,
-        'presion': 773.7371826,
-        'altitud': 2217.677002
-    }
-    return jsonify(data)
+    # Obtener los últimos datos enviados desde el ESP32
+    if datos_recibidos:
+        ultimo_dato = datos_recibidos[-1]
+        return jsonify(ultimo_dato)
+    else:
+        return jsonify({})
 
 @app.route('/')
 def index():
