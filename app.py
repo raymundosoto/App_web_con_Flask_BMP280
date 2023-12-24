c3925606976e7267fd7fb09dc3077b9344577783
Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2023-12-24 14:34:01 -0600

    Update app.py
    se modifica la app flask

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 from flask import Flask, request, jsonify, render_template
-import json
 
 app = Flask(__name__)
 
 temperaturas = []
 presiones = []
-altitudes = []
 
 @app.route('/datos', methods=['POST'])
 def recibir_datos():
     data = request.get_json()
     print("Datos recibidos:", data)
     
-    id_sensor = data['id_sensor']
-    timestamp = data['timestamp']
-    temperatura = data['temperatura']
-    presion = data['presion']
-    altitud = data['altitud']
+    temperatura = data.get('temperatura')
+    presion = data.get('presion')
 
     # Guardar datos para la gráfica
-    temperaturas.append(temperatura)
-    presiones.append(presion)
-    altitudes.append(altitud)
+    if temperatura is not None and presion is not None:
+        temperaturas.append(temperatura)
+        presiones.append(presion)
 
     return jsonify({"mensaje": "Datos recibidos correctamente"})
+
+@app.route('/grafica')
+def obtener_datos_grafica():
+    # Aquí puedes proporcionar los datos almacenados en temperaturas y presiones
+    # En este ejemplo, se devuelven los últimos 10 valores de temperatura y presión
+    datos_grafica = {
+        "temperatura": temperaturas[-10:],
+        "presion": presiones[-10:]
+    }
+    return jsonify(datos_grafica)
 
 @app.route('/')
 def index():
